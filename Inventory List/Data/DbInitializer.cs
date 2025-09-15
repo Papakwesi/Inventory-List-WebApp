19e68faff0bf70dc3f42b855261402bfd028610b
charles <charlestoku.125@gmail.com>
2025-09-15 20:23:12 +0000

    Updated DbInitializer

--- Inventory List/Data/DbInitializer.cs
+++ Inventory List/Data/DbInitializer.cs
@@ -19,37 +19,6 @@ namespace Inventory_List.Data
                     await roleManager.CreateAsync(new IdentityRole(role));
                 }
             }
-
-            // 2. Create default SuperAdmin user if not exists
-            var superAdminEmail = "superadmin@inventory.com";
-            var defaultPassword = "Superadmin@123";
-
-            var superAdmin = await userManager.FindByEmailAsync(superAdminEmail);
-
-            if (superAdmin == null)
-            {
-                superAdmin = new ApplicationUser
-                {
-                    UserName = superAdminEmail,
-                    Email = superAdminEmail,
-                    EmailConfirmed = true
-                };
-
-                var result = await userManager.CreateAsync(superAdmin, defaultPassword);
-
-                if (result.Succeeded)
-                {
-                    await userManager.AddToRoleAsync(superAdmin, "SuperAdmin");
-                }
-            }
-            else
-            {
-                // Make sure user has the SuperAdmin role
-                if (!await userManager.IsInRoleAsync(superAdmin, "SuperAdmin"))
-                {
-                    await userManager.AddToRoleAsync(superAdmin, "SuperAdmin");
-                }
-            }
         }
     }
 }
